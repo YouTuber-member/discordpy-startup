6d6906fec8e4de043bc46a2901738cc5feb82c70
howling-inferno <66354085+howling-inferno@users.noreply.github.com>
2020-07-12 16:50:10 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,14 +17,7 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
     
-@bot.command()
-async def pinga(ctx):
-    await ctx.send('pong')
-    
-    
-    @bot.command()
-async def add(ctx):
-    await ctx.send(a+b)
+
 
     
     
