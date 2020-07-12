9d3d634f5065464ee4a1fd0d40b46fab430ab10b
howling-inferno <66354085+howling-inferno@users.noreply.github.com>
2020-07-12 16:57:03 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,7 +17,19 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
     
+@bot.command() 
+async def join(ctx):
+    """Botをボイスチャンネルに入室させます。"""
+    voice_state = ctx.author.voice
 
+    if (not voice_state) or (not voice_state.channel):
+        #もし送信者がどこのチャンネルにも入っていないなら
+        await ctx.send("先にボイスチャンネルに入っている必要があります。")
+        return
+
+    channel = voice_state.channel #送信者のチャンネル
+
+    await channel.connect() #VoiceChannel.connect()を使用
 
     
     
